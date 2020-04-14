dc18c3bc3de5877c7ca2e31645333f21dee02b1a
eqs <murashige.satoshi.mi1@is.naist.jp>
2020-04-14 22:42:21 +0900

    Remove redundant overrides

--- examples/example1.py
+++ examples/example1.py
@@ -20,39 +20,6 @@ class ofApp(of.ofPyBaseApp):
     def draw(self):
         of.ofDrawCircle(self.x, self.y, 40)
 
-    def keyPressed(self, key):
-        pass
-
-    def keyReleased(self, key):
-        pass
-
-    def mouseMoved(self, x, y):
-        pass
-
-    def mouseDragged(self, x, y, button):
-        pass
-
-    def mousePressed(self, x, y, button):
-        pass
-
-    def mouseReleased(self, x, y, button):
-        pass
-
-    def mouseEntered(self, x, y):
-        pass
-
-    def mouseExited(self, x, y):
-        pass
-
-    def windowResized(self, w, h):
-        pass
-
-    def dragEvent(self, e):
-        pass
-
-    def gotMessage(self, msg):
-        pass
-
 
 if __name__ == '__main__':
     app = ofApp()
